e5eab827762dd2fb717a19c08b403326a03792f8
Millie <millie_clare@outlook.com>
2019-04-29 13:59:44 +0100

    includes 'bang'

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,22 +2,27 @@
 
 // Here, we create our main function.
 function fizzbuzz() {
-
     console.log('Welcome to FizzBuzz!');
     
-    for (let i = 0; i <= 100; i++) {
-        if (i % 3 === 0 && i % 5 === 0) {
-            console.log('FizzBuzz');
-        } else if (i % 3 === 0) {
-            console.log('Fizz');
-        } else if (i % 5 === 0) {
-            console.log("Buzz");
-        } else
-            (console.log(i));
+    for (let i = 1; i <= 100; i++) {
+        let nextNumber = '';
+        if(i % 3 === 0) {
+            nextNumber = 'Fizz';
+        } 
+        if(i % 5 === 0) {
+            nextNumber += 'Buzz';
+        } 
+        if(i % 7 === 0){
+            nextNumber += 'Bang';
+        }
+        if(nextNumber === ''){
+            console.log(i)
+        }
+        console.log(nextNumber)
     }
-
-
 }
-
 // Now we run the main function...
 fizzbuzz();
+//If a number is a multiple of 7, print "Bang" instead of the number. 
+//For numbers which are multiples of seven and three / five, append Bang
+//to what you'd have printed anyway. (E.g. 3 * 7 = 21: "FizzBang").
