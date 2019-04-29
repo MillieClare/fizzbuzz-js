98b07ee0882fc964163d9f8ae07722f670031303
Millie <millie_clare@outlook.com>
2019-04-29 12:34:10 +0100

    remove comments

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,20 +4,15 @@
 function fizzbuzz() {
 
     console.log('Welcome to FizzBuzz!');
-
-    // Put your code here...
+    
     for (let i = 0; i <= 100; i++) {
         if (i % 3 === 0 && i % 5 === 0) {
-            // if multiple of 3 & 5
             console.log('FizzBuzz');
         } else if (i % 3 === 0) {
-            // if multiple of 3
             console.log('Fizz');
         } else if (i % 5 === 0) {
-            // if multiple of 5
             console.log("Buzz");
         } else
-            // if anything else
             (console.log(i));
     }
 
